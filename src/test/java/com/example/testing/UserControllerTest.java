ef3bd5ab16db73521a355ed8253d7c1ed216f977
Dzaki Buchori <dzakibuchori@gmail.com>
2020-10-16 10:31:46 +0700

    working code (really work!) - add mockito

--- src/test/java/com/example/testing/UserControllerTest.java
+++ src/test/java/com/example/testing/UserControllerTest.java
@@ -1,14 +1,37 @@
 package com.example.testing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class UserControllerTest {
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
     @Test
     public void testGetUserById() {
-        UserController userController = new UserController();
+        User u = new User();
+        u.setId(1L);
+        u.setUserName("some name");
+        when(userService.getById(1L)).thenReturn(u);
+
         User user = userController.get(1L);
-        assertEquals(1l, user.getId().longValue());
+        verify(userService).getById(1L);
+
+        assertEquals(1L, user.getId().longValue());
     }
 }
