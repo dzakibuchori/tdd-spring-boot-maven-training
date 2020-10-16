9e0efa8fe876a47c086443f972b733708f3d59f5
Dzaki Buchori <dzakibuchori@gmail.com>
2020-10-16 10:29:29 +0700

    remove unused annotation

--- src/main/java/com/example/testing/User.java
+++ src/main/java/com/example/testing/User.java
@@ -1,7 +1,6 @@
 package com.example.testing;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -10,7 +9,6 @@ import javax.persistence.Id;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 @Entity
 public class User {
     @Id
