eaf3bb36f3fb8858cd751c40b6bc8ec6e6fbacb1
Dzaki Buchori <dzakibuchori@gmail.com>
2020-10-16 09:08:14 +0700

    3. write working code (maybe)

---
+++ src/main/java/com/example/testing/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.testing;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello My World!";
+    }
+}
